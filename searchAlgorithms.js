988c9d3ed02dc427ab26ab4a55abf62446801d28
Matt Lane <lane.matthew@gmail.com>
2016-05-12 10:31:10 -0700

    modify tests

--- searchAlgorithms.js
+++ searchAlgorithms.js
@@ -3,7 +3,7 @@ Array.prototype.linearSearch = function(val){
 };
 
 Array.prototype.binarySearch = function(val){
-
+  
 };
 
 module.exports = { Array };
--- test/searchAlgorithmsSpec.js
+++ test/searchAlgorithmsSpec.js
@@ -7,31 +7,55 @@ let negativeArr = [-5,-4,-3,-2,-1];
 let decimalArr = [1.1, 2.4,5.5,5.6,5.7];
 
 describe("#linearSearch", function(){
+  
+  it("does not use indexOf", function() {
+    expect(positiveArr.linearSearch.toString()).to.not.contain('indexOf');
+  });
+
   it("finds items in an array of positive numbers", function(){
     expect(positiveArr.linearSearch(3)).to.equal(2);
   });
+  
   it("finds items in an array of negative numbers", function(){
     expect(negativeArr.linearSearch(-2)).to.equal(3);
   });
+  
   it("finds items in an array of non-integers", function(){
     expect(decimalArr.linearSearch(1.1)).to.equal(0);
   });
+  
   it("returns -1 when the item is not found", function(){
     expect(decimalArr.linearSearch(0)).to.equal(-1);
   });
+
 });
 
 describe("#binarySearch", function(){
+  
+  it("does not use indexOf", function() {
+    expect(positiveArr.binarySearch.toString()).to.not.contain('indexOf');
+  });
+  
   it("finds items in an array of positive numbers", function(){
-    expect(positiveArr.linearSearch(3)).to.equal(2);
+    expect(positiveArr.binarySearch(3)).to.equal(2);
   });
+  
   it("finds items in an array of negative numbers", function(){
-    expect(negativeArr.linearSearch(-2)).to.equal(3);
+    expect(negativeArr.binarySearch(-2)).to.equal(3);
   });
+  
   it("finds items in an array of non-integers", function(){
-    expect(decimalArr.linearSearch(1.1)).to.equal(0);
+    expect(decimalArr.binarySearch(1.1)).to.equal(0);
   });
+  
   it("returns -1 when the item is not found", function(){
-    expect(decimalArr.linearSearch(0)).to.equal(-1);
+    expect(decimalArr.binarySearch(0)).to.equal(-1);
   });
+
+  it("should find elements in large arrays quickly", function(done) {
+    this.timeout(500);
+    setTimeout(done, 300);
+    expect(decimalArr.binarySearch(0)).to.equal(-1);
+  });
+
 });

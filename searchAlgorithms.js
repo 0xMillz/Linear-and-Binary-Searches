ff65506ce36f5fcc42dcbde072feb76b66b85193
millsmcilroy <millsmcilroy@gmail.com>
2016-05-18 14:17:47 -0600

    wrote linear and binary searches from scratch

--- searchAlgorithms.js
+++ searchAlgorithms.js
@@ -1,9 +1,34 @@
 Array.prototype.linearSearch = function(val){
 
+  for (var i = 0 ; i < this.length; i++) {
+    if (this[i] === val)
+      return i;
+  }
+  return -1;
 };
 
 Array.prototype.binarySearch = function(val){
-  
+  var min = 0;
+  var max = this.length -1;
+  var currentIndex;
+  var currentElement;
+
+  while (min <= max) {
+    currentIndex = Math.floor((min +max) / 2);
+    currentElement = this[currentIndex];
+
+    if (currentElement > val) {
+      max = currentIndex -1;
+    }
+    else if (currentElement < val) {
+      min = currentIndex + 1;
+    }
+    else {
+      return currentIndex;
+    }
+  }
+
+  return -1;
 };
 
 module.exports = { Array };
